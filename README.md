a93f3b41e7eba6d6fb289a54fc6e2afa8391b01c
lucas eduardo chaves frota <lucasecf@gmail.com>
2013-12-01 16:02:22 -0300

    First commit. Adding basic project structure to git, with rspec and first hello world test running

---
+++ README.md
@@ -0,0 +1,8 @@
+REQUIREMENTS
+
+- install rspec gem:
+If not installed yet, in terminal, type "gem install rspec".
+
+
+- To run all the tests written, run the follow command in terminal, inside the project root folder:
+"rspec spec --format nested"
---
+++ spec/quake_log_parser_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe QuakeLogParser do
+
+  before :each do
+  end
+  
+  describe "hello world test" do
+    it "should pass" do
+      test = "hello world"
+      test.should be_an_instance_of String
+    end  
+  end
+  
+
+end
